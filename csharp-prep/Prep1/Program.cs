448dc95667d3061f2aaee6300b36c53fbe890bce
Yoooung23 <logan7design@gmail.com>
2026-04-24 09:47:00 -0600

    part one of prep

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
+        int x = 84;
         Console.WriteLine("Hello Prep1 World!");
-
+        Console.WriteLine(x);
     }
 }

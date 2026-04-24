5501bc584578b89bedacce5433cb7c98404854b4
Yoooung23 <logan7design@gmail.com>
2026-04-24 10:51:45 -0600

    practicing with if statements

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,12 +1,30 @@
 using System;
 using System.Data;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int x = 84;
+        int lyX = 6;
+
         Console.WriteLine("Hello Prep1 World!");
-        Console.WriteLine(x);
-    }
+
+        Console.Write("What is your favorite color? ");
+        string lyColor = Console.ReadLine();
+
+        Console.WriteLine($"{lyColor} is a fantastic color!");
+
+        Console.Write("Pick a number between 1-100 ");
+        int lyY = int.Parse(Console.ReadLine());
+
+        if (lyX > lyY)
+        {
+            Console.WriteLine($"My number {lyX} is greater than {lyY}");
+        }
+        else
+        {
+            Console.WriteLine($"{lyY} is greater than {lyX}");
+        }
+    }   
 }

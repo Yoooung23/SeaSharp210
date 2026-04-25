0dc436a24a146afbbc57c4c1f8a7be2db9eace5d
Yoooung23 <logan7design@gmail.com>
2026-04-25 13:19:04 -0600

    Added a fun extra spin

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,12 +9,31 @@ class Program
         //Logan Young
         //CSE 210, Prep #1 
         //Apr 25, 2026
+        string lyBlank = "";
         Console.WriteLine("What is your first name?");
         string lyNameFirst = Console.ReadLine();
 
         Console.WriteLine("What is your Last name?");
         string lyNameLast = Console.ReadLine();
 
+        Console.WriteLine(lyBlank);
+
         Console.WriteLine($"Your name is {lyNameLast}, {lyNameFirst} {lyNameLast}");
+
+        Console.WriteLine(lyBlank);
+
+        Console.WriteLine($"What is your favorite color, {lyNameFirst}?");
+        string lyColor = Console.ReadLine();
+
+        Console.WriteLine(lyBlank);
+
+        Console.WriteLine($"{lyColor} is a sickening color {lyNameFirst}!");
+        Console.WriteLine("You should be ashamed!");
+
+        Console.WriteLine(lyBlank);
+
+        Console.WriteLine("Just pulling your leg.");
+        Console.WriteLine($"Goodbye. You {lyColor} loving cutie pie. ;D");
+        Console.WriteLine(lyBlank);
     }   
 }

c72700b92204e7a495cd0f03e2b43c7e8e8f33a3
Yoooung23 <logan7design@gmail.com>
2026-04-25 13:04:49 -0600

    Completed the Prep one assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,25 +6,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        int lyX = 6;
+        //Logan Young
+        //CSE 210, Prep #1 
+        //Apr 25, 2026
+        Console.WriteLine("What is your first name?");
+        string lyNameFirst = Console.ReadLine();
 
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your Last name?");
+        string lyNameLast = Console.ReadLine();
 
-        Console.Write("What is your favorite color? ");
-        string lyColor = Console.ReadLine();
+        Console.WriteLine($"Your name is {lyNameLast}, {lyNameFirst} {lyNameLast}");
 
-        Console.WriteLine($"{lyColor} is a fantastic color!");
 
-        Console.Write("Pick a number between 1-100 ");
-        int lyY = int.Parse(Console.ReadLine());
 
-        if (lyX > lyY)
-        {
-            Console.WriteLine($"My number {lyX} is greater than {lyY}");
-        }
-        else
-        {
-            Console.WriteLine($"{lyY} is greater than {lyX}");
-        }
+        // int lyX = 6;
+
+        // Console.WriteLine("Hello Prep1 World!");
+
+        // Console.Write("What is your favorite color? ");
+        // string lyColor = Console.ReadLine();
+
+        // Console.WriteLine($"{lyColor} is a fantastic color!");
+
+        // Console.Write("Pick a number between 1-100 ");
+        // int lyY = int.Parse(Console.ReadLine());
+
+        // if (lyX > lyY)
+        // {
+        //     Console.WriteLine($"My number {lyX} is greater than {lyY}");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"{lyY} is greater than {lyX}");
+        // }
+        // Console.Write("It is Yoda Time!!!!!!!!!!!!!!!!!!!");
+        // string ly.Name = Console.ReadLine();
     }   
 }

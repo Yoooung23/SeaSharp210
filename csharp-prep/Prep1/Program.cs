24cb293d0b5f6cd20dbb6db3932d2f47a28ab1cb
Yoooung23 <logan7design@gmail.com>
2026-04-25 13:05:46 -0600

    Did some cleaning in the file

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -16,30 +16,5 @@ class Program
         string lyNameLast = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lyNameLast}, {lyNameFirst} {lyNameLast}");
-
-
-
-        // int lyX = 6;
-
-        // Console.WriteLine("Hello Prep1 World!");
-
-        // Console.Write("What is your favorite color? ");
-        // string lyColor = Console.ReadLine();
-
-        // Console.WriteLine($"{lyColor} is a fantastic color!");
-
-        // Console.Write("Pick a number between 1-100 ");
-        // int lyY = int.Parse(Console.ReadLine());
-
-        // if (lyX > lyY)
-        // {
-        //     Console.WriteLine($"My number {lyX} is greater than {lyY}");
-        // }
-        // else
-        // {
-        //     Console.WriteLine($"{lyY} is greater than {lyX}");
-        // }
-        // Console.Write("It is Yoda Time!!!!!!!!!!!!!!!!!!!");
-        // string ly.Name = Console.ReadLine();
     }   
 }

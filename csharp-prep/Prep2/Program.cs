5a64ead7ab342fa0d6e808569d64177a410f2d11
Yoooung23 <logan7design@gmail.com>
2026-04-25 19:02:05 -0600

    working on the strech activity.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("Enter a number ");
+        string valueFromUser = Console.ReadLine();
+        int grade = int.Parse(valueFromUser);
+
+        int x = grade;
+        int y = 2;
+        int z = 5;
+
+        // Or = ||, And = &&
+        if (x > y || x > z)
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x < y)
+        {
+            Console.WriteLine("Less");
+        }
+        else
+        {
+            Console.WriteLine("Equal");
+        }
+
+        int gradeA = 90;
+        int gradeB = 80;
+        int gradeC = 70;
+        int gradeD = 60;
+        int gradeF = 0;
+
+        int number = 
+        string lyTxtVersion = Number.ToString
+
+        Console.Write("Please provide you garde percentage: ");
+        string lyUserValue = Console.ReadLine();
+        float gradePercent = float.Parse(lyUserValue);
+
+        Console.WriteLine($"You have provided {gradePercent} as your grade percentage.");
+        Console.WriteLine($"which means you have a letter grade of {lyStudentgrade}.");
 }

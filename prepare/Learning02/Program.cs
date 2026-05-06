52eb7ad51fe264e043cc8bfef3619b60be9313fc
Yoooung23 <logan7design@gmail.com>
2026-05-06 13:25:40 -0600

    Added some drama.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Console.Write("do you want to continue? ");
+            response = Console.ReadLine();
+        }
+        Console.WriteLine("*dramatic music* It is... finished.... *drums* bum bum BOM!");
     }
 }

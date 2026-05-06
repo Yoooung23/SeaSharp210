a9786dcbe890ccc9dc9b3d972fb0bc3f7ef0f15f
Yoooung23 <logan7design@gmail.com>
2026-05-06 14:41:26 -0600

    made a cool loop

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,16 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Random rand = new Random();
         string response = "yes";
 
         while (response == "yes")
         {
-            Console.Write("do you want to continue? ");
-            response = Console.ReadLine();
+            List<double> numbers = new List<double>();
+
+            // Generate random numbers
+            for (int i = 0; i < 10; i++)
+            {
+                double randomNumber = rand.NextDouble() * 1000;
+                numbers.Add(randomNumber);
+            }
+
+            Console.WriteLine("\nGenerated numbers:");
+            foreach (double num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            double lowest = numbers[0];
+
+            foreach (double num in numbers)
+            {
+                if (num < lowest)
+                {
+                    lowest = num;
+                    Console.WriteLine($"Set to: [{lowest}]");
+                }
+            }
+
+            Console.WriteLine($"\nThe lowest number is: {lowest}");
+
+            Console.Write("\nDo you want to continue? ");
+            response = Console.ReadLine().ToLower();
         }
-        Console.WriteLine("*dramatic music* It is... finished.... *drums* bum bum BOM!");
+
+        Console.WriteLine("*dramatic music*");
+        Console.WriteLine("");
+        Console.WriteLine("It is... finished....");
+        Console.WriteLine("");
+        Console.WriteLine("*drums*");
+        Console.WriteLine("bum bum BOM!");
+        Console.WriteLine("");
     }
 }

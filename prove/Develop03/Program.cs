1acf0f64179851641ef56202247039bd324e7a6d
Yoooung23 <logan7design@gmail.com>
2026-06-11 21:02:43 -0600

    organizing files under the right sections of the course

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,46 +8,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Journal journal = new Journal();
-        bool running = true;
-        while (running)
-        {
-            // Display Menu
-            Console.Clear();
-            Console.WriteLine();
-            Console.WriteLine("Welcome to the Journal Menu");
-            Console.WriteLine("please select an option:");
-            Console.WriteLine("1) Write an entry");
-            Console.WriteLine("2) Read your past entries enter");
-            Console.WriteLine("3) Save & Exit");
-            Console.WriteLine();
-            string choice = Console.ReadLine();
+        // Journal journal = new Journal();
+        // bool running = true;
+        // while (running)
+        // {
+        //     // Display Menu
+        //     Console.Clear();
+        //     Console.WriteLine();
+        //     Console.WriteLine("Welcome to the Journal Menu");
+        //     Console.WriteLine("please select an option:");
+        //     Console.WriteLine("1) Write an entry");
+        //     Console.WriteLine("2) Read your past entries enter");
+        //     Console.WriteLine("3) Save & Exit");
+        //     Console.WriteLine();
+        //     string choice = Console.ReadLine();
 
-            if (choice == "1") //Write
-            {
-                journal.NewEntry();
-            }
-            else if (choice == "2") //Read
-            {
-                journal.DisplayEntries();
-                Console.Write("Please hit enter to proceed");
-                Console.ReadLine();
-            }
-            else if (choice == "3") //Save and Quit
-            {
-                journal.SaveEntries();
-                running = false; //<---Breakout point
-            }
-            else
-            {
-                Console.WriteLine("Invalid option. Try again.");
-            }
-        }
-        //Sign Off
-        Console.Clear();
-        Console.WriteLine();
-        Console.WriteLine("Program ended.");
-        Console.WriteLine("TTFN!");
-        Console.WriteLine();
+        //     if (choice == "1") //Write
+        //     {
+        //         journal.NewEntry();
+        //     }
+        //     else if (choice == "2") //Read
+        //     {
+        //         journal.DisplayEntries();
+        //         Console.Write("Please hit enter to proceed");
+        //         Console.ReadLine();
+        //     }
+        //     else if (choice == "3") //Save and Quit
+        //     {
+        //         journal.SaveEntries();
+        //         running = false; //<---Breakout point
+        //         // This is where I exceeded the expectations of the assignment, my saveEntries() method will save all the entries in my list toi a JSON file.
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Invalid option. Try again.");
+        //     }
+        // }
+        // //Sign Off
+        // Console.Clear();
+        // Console.WriteLine();
+        // Console.WriteLine("Program ended.");
+        // Console.WriteLine("TTFN!");
+        // Console.WriteLine();
     }
 }
